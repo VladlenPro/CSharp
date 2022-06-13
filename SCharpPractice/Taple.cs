8b74e97dc7e9ff8471083c0b45c4b6b8780d8e9e
Vlad Probst <vladlen.probst@gmail.com>
2022-06-13 15:36:21 +0300

    Taple example

---
+++ SCharpPractice/Taple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCharpPractice
+{
+    internal class Taple
+    {
+        public void Run()
+        {
+            var newPerson = CreatePerson();
+        }
+        public (int id, string fname, string lname) CreatePerson()
+        {
+            int id = 15;
+            string fname = "Vlad";
+            string lname =  "Probst";
+            (int id, string fname, string lname) var1;
+            var1.id = id;
+            var1.fname = fname;
+            var1.lname = lname;
+            return var1;
+
+        }
+    }
+}
