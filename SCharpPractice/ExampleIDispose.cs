867d69a2b53da922152053c2aa7a446fd7c9c9db
Vlad Probst <vladlen.probst@gmail.com>
2022-06-29 16:56:58 +0300

    check, as and is, IDisposable examples

---
+++ SCharpPractice/ExampleIDispose.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCharpPractice
+{
+    class MakeBigRequest : IDisposable
+    {
+        public void Dispose()
+        {
+            Release();
+        }
+
+        public void Operate()
+        {
+
+        }
+
+        public void Release()
+        {
+
+        }
+    }
+    internal class ExampleIDispose
+    {
+        public void Run()
+        {
+            //MakeBigRequest makeBigRequest = new MakeBigRequest();
+            //makeBigRequest.Operate();
+            //release resourses
+            //makeBigRequest.Release();
+            //or
+            //makeBigRequest.Dispose();
+            using (MakeBigRequest makeBigRequest = new MakeBigRequest())
+            {
+                makeBigRequest.Operate();
+            }
+            //using (var f = System.IO.File.Open())
+            //{
+
+            //}
+        }
+    }
+}
