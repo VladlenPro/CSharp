19b6257488c118a700452a8bd19716728c69d6a6
Vlad Probst <vladlen.probst@gmail.com>
2022-07-06 19:16:19 +0300

    Serialize desrialize

---
+++ SCharpPractice/JsonSmapleClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCharpPractice
+{
+    internal class JsonSmapleClass
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string schemaVersion { get; set; }
+        public string testVersion { get; set; }
+        public string previousVersionId { get; set; }
+        public string authorId { get; set; }
+        public string description { get; set; }
+        public string[] tags { get; set; }
+        public string[] questionsRefs
+        {
+            get; set;
+        }
+
+    }
+
+}
